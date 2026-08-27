7b6841857d74c4a781b1a96ddf8b43da86fb1076
Poorneswar Yalavarthi <poorneswar2020@gmail.com>
2026-08-27 10:58:30 +0530

    Add integer and float division logic
    Implement integer and float division for two input integers.

---
+++ DivisionOperator.py
@@ -0,0 +1,19 @@
+'''
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . 
+The second line should contain the result of float division,  / .
+
+No rounding or formatting is necessary.
+
+Input: 
+a=3
+b=5
+Output: 
+0
+0.6
+'''
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
